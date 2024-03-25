34532b75744f5ce8a9084f968290d4188c0aa8eb
wacista <wacista@student.42.fr>
2024-03-25 16:37:26 +0100

    fix c06

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: wacista <wacista@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 13:03:53 by wacista           #+#    #+#             */
-/*   Updated: 2024/03/25 16:19:07 by wacista          ###   ########.fr       */
+/*   Updated: 2024/03/25 16:33:52 by wacista          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,20 @@ void	print_av(char *av[])
 	}
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
 int	main(int ac, char *av[])
 {
 	int		i;
@@ -43,7 +57,7 @@ int	main(int ac, char *av[])
 		j = i + 1;
 		while (j < ac)
 		{
-			if (av[i][0] > av[j][0])
+			if (ft_strcmp(av[i], av[j]) > 0)
 			{
 				tmp = av[i];
 				av[i] = av[j];
